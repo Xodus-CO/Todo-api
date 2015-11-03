002ea6fcacd91e9216832e10231403fc3827f47d
Nathan Mead <nmead33@gmail.com>
2015-11-02 22:37:32 -0700

    Init repo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
 	res.send('Todo API root');
 });
 
-app.listen(PORT, function(){
-	console.log('Express listening on ' + PORT );
+app.listen(PORT, function() {
+	console.log('Express listening on ' + PORT + '!');
 });
 
