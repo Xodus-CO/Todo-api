c4b1115b04ea99df0f78df8b9e785539f159a9e6
Nathan Mead <nmead33@gmail.com>
2015-11-03 19:29:18 -0700

    Add variable playground file

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// // 	obj = {
+// // 		name: 'Andrew',
+// // 		age: 24
+// // 	};
+// // }
+// obj.age = 24;
+// }
+
+// updatePerson(person);
+
+// console.log(person);
+
+//  Array example
+
+var grades = [15, 37];
+
+function addGrades(grade) {
+	grades.push(grade);
+	debugger;
+}
+
+addGrades(45);
+console.log(grades);
