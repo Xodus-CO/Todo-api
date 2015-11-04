a736fb60a7ec0dc3e5d9529e7d283b1f1d221e08
Nathan Mead <nmead33@gmail.com>
2015-11-03 21:15:37 -0700

    Add sequelize demo

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/todos', function (req, res) {
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if  (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') { 
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') { 
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
 	
