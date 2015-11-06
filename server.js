ebd79997005b525d8e3cca5a44ed67a9cc3e6781
Nathan Mead <nmead33@gmail.com>
2015-11-06 12:06:58 -0700

    Hash and Salt password

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.post ('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function (user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 
@@ -132,7 +132,7 @@ app.post ('/users', function (req, res) {
 
 
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force:false}).then(function () {
 	app.listen(PORT, function () {
 		console.log('Express listening on port ' + PORT + '!');
 	});
